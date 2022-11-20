eb724a63225686487728e13ab3a81d93668c8941
Taha Hassan <tahafhassan@gmai.com>
2022-11-20 15:46:51 +0000

    added parallax scrolling to intro section

--- f&cwebsite.js
+++ f&cwebsite.js
@@ -10,3 +10,14 @@ function showHide(button) {
         toHideShow[0].classList.toggle('show');
     }
 }
+
+let sky = document.getElementById("sky");
+let building = document.getElementById("building");
+let text = document.getElementById('h1text');
+
+window.addEventListener('scroll', function(){
+    var yValue = this.window.scrollY;
+    sky.style.top = yValue + 0.1 + "px";
+    text.style.left = yValue + 1 + "px";
+    text.style.top = yValue + 0.1 + "px";
+})
